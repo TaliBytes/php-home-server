0cb68b4c294983733b5872479bbddc87868fa25a
talibytes <shaf.talia1@gmail.com>
2024-12-08 19:05:55 -0700

    Began foundation of website. Includes global variables, route handling, start of styling.

---
+++ root/page_notFound.php
@@ -0,0 +1,25 @@
+<?php
+    include 'globals.php';
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Not Found</title>
+        <link rel="stylesheet" href="/static/globalStyles.css"/>
+    </head>
+    <body>
+        <?php
+            if($globalAlertMsg != null) {echo "<div id='globalAlertBanner'>$globalAlertMsg</div>";}
+        ?>
+
+        <div style="display:block; width:100%; min-height:108px; font-size:1.3rem; text-align:center; color:white; font-weight:bold; background-color:rgb(0, 66, 110);">
+            <br/><span>An Error Has Occurred</span><br/><br/>
+            <span style="display:block; width:95%; margin:auto; font-weight:normal; font-size:1.2rem;">
+                The page you requested could not be found and a website technician has been notified. If you typed the link (URL) into your browser directly, please check it for accuracy.
+            </span><br/>
+        </div>
+    </body>
+</html> 
